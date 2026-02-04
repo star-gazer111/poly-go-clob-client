90537f658429cea5019a91d2f0e9b7e55e10587c
star-gazer111 <vasukhanna961@gmail.com>
2026-02-04 09:09:59 +0530

    added public client

--- clob/l1_client.go
+++ clob/l1_client.go
@@ -1,17 +1,20 @@
 package clob
 
-import (
-	"github.com/star-gazer111/poly-go-clob-client/auth"
-)
+import "github.com/star-gazer111/poly-go-clob-client/auth"
 
 type L1Client struct {
 	*PublicClient
 	signer auth.Signer
 }
 
-func NewL1Client(baseURL string, signer auth.Signer, opts ...PublicClientOption) *L1Client {
-	return &L1Client{
-		PublicClient: NewPublicClient(baseURL, opts...),
-		signer:       signer,
+func NewL1Client(baseURL string, signer auth.Signer, opts ...PublicClientOption) (*L1Client, error) {
+	pc, err := NewPublicClient(baseURL, opts...)
+	if err != nil {
+		return nil, err
 	}
+
+	return &L1Client{
+		PublicClient: pc,
+		signer:       signer,
+	}, nil
 }
